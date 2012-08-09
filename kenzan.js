313a1162afde16e02b3b38b90622f21c662bdff2
Kenzan Boo <kenzanboo@gmail.com>
2012-08-09 11:43:01 -0700

    scaffold up with changing cover picture

--- kenzan.js
+++ kenzan.js
@@ -1,15 +1,17 @@
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to Kenzan Boo's site. Currently being migrated to Metoer...";
-  };
+    var number_of_cover_backgrounds = 9;
 
-  Template.hello.events = {
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  };
+    Template.cover.bg_num = function () {
+        return Math.round(Math.random() * number_of_cover_backgrounds);
+    };
+
+    Template.hello.events = {
+        'click input' : function () {
+        // template data, if any, is available in 'this'
+        if (typeof console !== 'undefined')
+            console.log("You pressed the button");
+        }
+    };
 }
 
 if (Meteor.is_server) {
